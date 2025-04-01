e92ef0a2a74ef8409d34426267102ee7876d5efd
Mujtaba Badar <mujbadar@macbook-pro-6.lan>
2025-03-31 22:34:12 -0500

    up keyword token

--- controllers/askController.js
+++ controllers/askController.js
@@ -62,7 +62,7 @@ Maintain a respectful, scholarly tone and focus on authentic sources.`,
           },
           { role: "user", content: question },
         ],
-        max_tokens: 50,
+        max_tokens: 800,
         temperature: 0.2,
       },
       {
